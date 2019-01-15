f4b391bf954e4ce0ff1c422214483a157f979baa
Robert Vidal <bobbyvidal@gmail.com>
2019-01-15 01:15:43 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,14 @@
 def alphabetize(arr)
    # code here
+   array.sort_by do |word|
+    i = 0
+    sort_by_array = []
+
+    word.length.times do
+      sort_by_array << esperanto_alphabet.index(word[i])
+      i += 1
+    end
+
+    sort_by_array
+  end
 end
